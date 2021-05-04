5164a2722f5e6bace59b1b8497f47d69bd980e80
Jas802 <jas.spencer414@gmail.com>
2021-05-04 12:41:24 -0400

    displayed price to product

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,14 +12,16 @@ function Product({ product }) {
         <a href={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
-
-            <Card.Text as='div'>
-              <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-              </div>
-            </Card.Text>
           </Card.Title>
         </a>
+
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
