5ecd0e9af1ec83513cfc86f33efe603b7a610904
Jas802 <jas.spencer414@gmail.com>
2021-06-07 12:16:11 -0400

    login form with submit handler

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -11,7 +11,40 @@ function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  return <FormContainer>Login Screen</FormContainer>;
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('submitted!');
+  }
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='priamry'>Sign In</Button>
+      </Form>
+    </FormContainer>
+  );
 }
 
 export default LoginScreen;
