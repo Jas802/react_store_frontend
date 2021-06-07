b8ace87930105491da48d2857395f44d8616f74f
Jas802 <jas.spencer414@gmail.com>
2021-06-07 12:27:39 -0400

    redirect and useDispath on login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
@@ -7,19 +7,32 @@ import Message from '../components/Message';
 import { login } from '../actions/userActions';
 import FormContainer from '../components/FormContainer'
 
-function LoginScreen() {
+function LoginScreen({location, history}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch()
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const userLogin = useSelector(state => state.userLogin)
+  const {error, loading, userInfo} = userLogin
+
+  useEffect(() => {
+    if(userInfo){
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log('submitted!');
+    dispatch(login(email, password))
   }
   return (
     <FormContainer>
       <h1>Sign In</h1>
-
+      {error && <Message variant='danger'></Message>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
@@ -43,6 +56,11 @@ function LoginScreen() {
 
         <Button type='submit' variant='priamry'>Sign In</Button>
       </Form>
+      <Row className='py-3'>
+        <Col>
+          New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+        </Col>
+      </Row>
     </FormContainer>
   );
 }
