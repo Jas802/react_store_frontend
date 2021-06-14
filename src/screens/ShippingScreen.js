18041553c4a3beacb5425f7f474a35e1e662ab05
Jas802 <jas.spencer414@gmail.com>
2021-06-14 12:40:41 -0400

    component outline for payment screen

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -27,7 +27,7 @@ function ShippingScreen({ history }) {
 
   return (
     <FormContainer>
-      <CheckoutSteps />
+      <CheckoutSteps step1 step2/>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
