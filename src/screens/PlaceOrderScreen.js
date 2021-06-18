532417a869d0bfc93dbde4998262a255904c7406
Jas802 <jas.spencer414@gmail.com>
2021-06-18 10:53:20 -0400

    displayed shipping, payment, and items on orderscreen

--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -9,9 +9,64 @@ function PlaceOrderScreen() {
   const cart = useSelector(state => state.cart)
   return (
     <div>
-      <CheckoutSteps step1 step3 step3 step4 />
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+
+              <p>
+                <strong>Shipping: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}
+                {'   '}
+                {cart.shippingAddress.postalCode},{'   '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+
+              <p>
+                <strong>Method: </strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+                {cart.cartItems.length === 0 ? <Message variant='info'>
+                  Your cart is empty
+                </Message> : (
+                  <ListGroup variant='flush'>
+                    {cart.cartItems.map((item, index) => (
+                      <ListGroup.Item key={index}>
+                        <Row>
+                          <Col md={1}>
+                            <Image src={item.image} alt={item.name} fluid rounded/>
+                          </Col>
+
+                          <Col>
+                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                          </Col>
+
+                          <Col md={4}>
+                            {item.qty} X ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={4}></Col>
+      </Row>
     </div>
-  )
+  );
 }
 
 export default PlaceOrderScreen
