ef21203a8eb2e41ff8c89bf859d0d03862ab9b49
Jas802 <jas.spencer414@gmail.com>
2021-06-24 13:42:21 -0400

    placing order clears items from cart

--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps';
 import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
 
 function PlaceOrderScreen({history}) {
   
@@ -23,13 +24,14 @@ function PlaceOrderScreen({history}) {
     Number(cart.shippingPrice) +
     Number(cart.taxPrice)).toFixed(2)
 
-  if(cart.paymentMethod){
+  if(!cart.paymentMethod){
     history.push('/payment')
   }
 
   useEffect(() => {
     if(success){
       history.push(`/order/${order._id}`)
+      dispatch({ type: ORDER_CREATE_RESET })
     }
   }, [success, history])
 
